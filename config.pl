83ef2976a75a735d680f0a6b76e6f568c2fc1c85
ito-yuma <ito-yuma@kayac.com>
2016-04-28 14:20:33 +0900

    Arkのモデル

--- config.pl
+++ config.pl
@@ -1,3 +1,11 @@
-+{
-    default_view    => 'MT',
-}
+my $home = Jobeet::Models->get('home');
+
+return {
+  database => [
+    'dbi:mysql:arktest','root',
+    {
+      mysql_enable_utf8 => 1,
+      on_connect_do => ['SET NAMES utf8'],
+    },
+  ],
+};
