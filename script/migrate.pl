9a0263e75b66038aba079ec778b2acedced6fa89
ito-yuma <ito-yuma@kayac.com>
2016-04-28 14:21:55 +0900

    スキーマを作るスクリプト

---
+++ script/migrate.pl
@@ -0,0 +1,37 @@
+use strict;
+use warnings;
+use utf8;
+use lib 'lib';
+
+use Jobeet::Models;
+use Getopt::Long;
+use GitDDL;
+
+GetOptions(
+  \my %options,
+  qw/dry-run/
+);
+
+my $dsn = models('conf')->{database}->[0];
+my $gd = GitDDL->new(
+  work_tree => './',
+  ddl_file => './sql/schema.sql',
+  dsn => models('conf')->{database},
+);
+
+my $db_version = '';
+eval {
+  open my $fh, '>',\my $stderr;
+  local *STDERR = $fh;
+  $db_version = $gd->database_version;
+};
+
+if($gd->check_version){
+  say 'Database is already latest';
+}else{
+  print $gd->diff . "\n";
+  if(!$options{'dry-run'}){
+    $gd->upgrade_database;
+    say 'done migrate';
+  }
+}
