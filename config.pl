e7102393e0560d777d5eae5714bc11df953b1124
ito-yuma <ito-yuma@kayac.com>
2016-05-06 16:39:45 +0900

    configとModel.pmを書き換えた

--- config.pl
+++ config.pl
@@ -1,15 +1,20 @@
-# my $home = Jobeet::Models->get('home');
+my $home = Jobeet::Models->get('home');
 # print "config\n";
 return {
-    default_view => 'MT',
-    active_days => 30,
+    default_view         => 'MT',
+    active_days          => 30,
     max_jobs_on_homepage => 10,
     max_jobs_on_category => 20,
-    database => [
-        'dbi:mysql:arktest','root','',
-         {
-             mysql_enable_utf8 => 1,
-             on_connect_do     => ['SET NAMES utf8'],
-         },
+    database             => [
+        'dbi:mysql:arktest',
+        'root', '',
+        {
+            mysql_enable_utf8 => 1,
+            on_connect_do     => ['SET NAMES utf8'],
+        },
     ],
+    cache => {
+        share_file     => $home->file( 'tmp', 'cache' )->stringify,
+        unlink_on_exit => 0,
+    },
 };
--- lib/Jobeet/Models.pm
+++ lib/Jobeet/Models.pm
@@ -2,17 +2,27 @@ package Jobeet::Models;
 use strict;
 use warnings;
 use Ark::Models '-base';
-
+use Cache::FastMmap;
 
 register Schema => sub {
     my $self = shift;
 
     my $conf = $self->get('conf')->{database}
-        or die 'require database config';
+      or die 'require database config';
 
     $self->ensure_class_loaded('Jobeet::Schema');
+
     # print "connect mysql\n";
     Jobeet::Schema->connect(@$conf);
+};
+
+register cache => sub {
+    my $self = shift;
+
+    my $conf = $self->get('conf')->{cache} or die 'reqiure cache config';
+
+    $self->ensure_class_loaded('Cache::FastMmap');
+    Cache::FastMmap->new(%$conf);
 };
 
 for my $table (qw/Job Category CategoryAffiliate Affiliate/) {
@@ -23,11 +33,11 @@ for my $table (qw/Job Category CategoryAffiliate Affiliate/) {
 }
 
 autoloader qr/^Schema::/ => sub {
-    my ($self, $name) = @_;
+    my ( $self, $name ) = @_;
 
     my $schema = $self->get('Schema');
-    for my $t ($schema->sources) {
-        $self->register( "Schema::$t" => sub { $schema->resultset($t) });
+    for my $t ( $schema->sources ) {
+        $self->register( "Schema::$t" => sub { $schema->resultset($t) } );
     }
 };
 
