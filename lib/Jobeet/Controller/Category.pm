cd3784c902872ab34ba47532e8e0007f84ab1e48
ito-yuma <ito-yuma@kayac.com>
2016-05-06 15:28:03 +0900

    タイポ修正「default」

--- lib/Jobeet/Controller/Category.pm
+++ lib/Jobeet/Controller/Category.pm
@@ -6,7 +6,7 @@ sub show :Path :Args(1) {
   my ($self,$c,$category_name) = @_;
 
   my $category = models('Schema::Category')->find({slug => $category_name})
-    or $c->detach('/defaut');
+    or $c->detach('/default');
 
   $c->stash->{category} = $category;
   $c->stash->{jobs} = $category->get_active_jobs({
