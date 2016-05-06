610f96df2f770eae5bda725d44d6bf4e1badaafa
ito-yuma <ito-yuma@kayac.com>
2016-05-06 16:44:54 +0900

    sessionを使う準備

--- lib/Jobeet.pm
+++ lib/Jobeet.pm
@@ -5,6 +5,22 @@ use_model 'Jobeet::Models';
 use Data::Page::Navigation;
 our $VERSION = '0.01';
 
+use_plugins qw{
+  Session
+  Session::State::Cookie
+  Session::Store::Model
+};
+
+config 'Plugin::Session' => {
+  expires => '+30d',
+};
+config 'Plugin::Session::State::Cookie' => {
+  cookie_name => 'jobeet_session',
+};
+config 'Plugin::Session::Store::Model' => {
+  model => 'cache',
+}
+
 __PACKAGE__->meta->make_immutable;
 
 __END__
