367ce703a263a5aad455d14e7f758f618e624470
ito-yuma <ito-yuma@kayac.com>
2016-05-06 17:41:08 +0900

    フィード

--- lib/Jobeet/Controller/Job.pm
+++ lib/Jobeet/Controller/Job.pm
@@ -3,6 +3,7 @@ use Ark 'Controller';
 with 'Ark::ActionClass::Form';
 
 use Jobeet::Models;
+use DateTime::Format::W3CDTF;
 
 # use Data::Dumper;
 
@@ -87,6 +88,16 @@ sub delete : Chained('job') : PathPart : Args(0) {
 
     $c->stash->{job}->delete;
     $c->redirect( $c->uri_for('/job') );
+}
+
+sub atom :Local {
+  my ($self,$c) = @_;
+  $c->res->content_type('application/atom+xml; charset=utf-8');
+
+  $c->stash->{w3c_date} = DateTime::Format::W3CDTF->new;
+  $c->stash->{latest_post} = models('Schema::Job')->latest_post;
+
+  $c->forward('index');
 }
 
 1;
--- lib/Jobeet/Schema/ResultSet/Job.pm
+++ lib/Jobeet/Schema/ResultSet/Job.pm
@@ -7,20 +7,29 @@ use parent 'DBIx::Class::ResultSet';
 use Jobeet::Models;
 
 sub create_from_form {
-  my ($self,$form) = @_;
+    my ( $self, $form ) = @_;
 
-  my $txn_guard = models('Schema')->txn_scope_guard;
+    my $txn_guard = models('Schema')->txn_scope_guard;
 
-  my $category_id = delete $form->params->{category};
-  my $category = models('Schema::Category')->find({slug => $category_id})
-    or die 'no such category_id:',$category_id;
+    my $category_id = delete $form->params->{category};
+    my $category = models('Schema::Category')->find( { slug => $category_id } )
+      or die 'no such category_id:', $category_id;
 
-  my $job = $self->create({
-    category_id=>$category->id,
-    %{ $form->params },
-  });
-  $txn_guard->commit;
-  $job;
+    my $job = $self->create(
+        {
+            category_id => $category->id,
+            %{ $form->params },
+        }
+    );
+    $txn_guard->commit;
+    $job;
+}
+
+sub latest_post {
+    my ($self) = @_;
+    my $r = $self->search( { is_activated => 1, },
+        { order_by => { -desc => 'created_at' } } );
+    $r->first;
 }
 
 1;
